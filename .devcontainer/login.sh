de40272249b30c507b061222cb2fc923d6b747d6
Ryoji Ban <r3b3a0n@gmail.com>
2020-08-12 20:09:42 +0900

    パスワードを管理できるように

---
+++ .devcontainer/login.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# AtCoderのツール類のログインを行うスクリプト
+
+if [ $# -ne 2 ]; then
+  echo "指定された引数は$#個です。"
+  echo "sh login.sh <USER_NAME> <PASSWORD>"
+  exit 1
+fi
+
+USER=$1
+PASS=$2
+
+echo "🚀🚀🚀Login ACC"
+expect -c "
+set timeout 10
+spawn acc login
+expect \"username:\"
+send \"${USER}\n\"
+expect \"password:*\"
+send \"${PASS}\n\"
+expect \"OK\"
+exit 0
+"
+
+echo "🚀🚀🚀Login ACC"
+expect -c "
+spawn oj login https://beta.atcoder.jp/
+expect \"Username:\" 
+send \"${USER}\n\"
+expect \"Password:\"
+send \"${PASS}\n\"
+expect \"*Welcome*\"
+exit 0
+"
