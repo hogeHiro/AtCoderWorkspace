7b86812f871bc19e08daeba03484a17c6a1a4ed4
Ryoji Ban <r3b3a0n@gmail.com>
2020-08-15 13:21:10 +0900

    abc-085-cを解く

--- workspace/abc085/c/main.py
+++ workspace/abc085/c/main.py
@@ -2,7 +2,19 @@
 
 
 def main():
-    pass
+    args = input().split()
+    N = int(args[0])
+    Y = int(args[1])
+
+    for num10000 in range(0, N + 1):
+        for num5000 in range(0, N + 1 - num10000):
+            num1000 = N - num10000 - num5000
+            value = 10000 * num10000 + 5000 * num5000 + 1000 * num1000
+            if value == Y:
+                print(num10000, num5000, num1000)
+                return
+
+    print(-1, -1, -1)
 
 
 if __name__ == "__main__":
