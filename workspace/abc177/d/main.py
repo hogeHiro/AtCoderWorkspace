bd0df38cc49f0c57c26cb89609310d4d95e8989b
Ryoji Ban <r3b3a0n@gmail.com>
2020-08-30 03:26:34 +0900

    途中が来

--- workspace/abc177/d/main.py
+++ workspace/abc177/d/main.py
@@ -12,28 +12,23 @@ def main():
     N = int(args[0])
     M = int(args[1])
 
-    groupList = list()
+    groupTable = [i for i in range(N + 1)]
+
     for i in range(M):
         args = list(map(lambda x: int(x), input().split()))
-
-        isHit = False
-        for group in groupList:
-            if args[0] in group:
-                group.add(args[1])
-                isHit = True
-            elif args[1] in group:
-                group.add(args[0])
-                isHit = True
-
-        if not isHit:
-            groupList.append({args[0], args[1]})
+        if groupTable[args[0]] > groupTable[args[1]]:
+            groupTable[args[0]] = groupTable[args[1]]
+        else:
+            groupTable[args[1]] = groupTable[args[0]]
 
     maxNum = 0
-    for group in groupList:
-        maxNum = max(maxNum, len(group))
+    for i in set(groupTable):
+        counter = groupTable.count(i)
+        maxNum = max(maxNum, counter)
 
     print(maxNum)
 
 
 if __name__ == "__main__":
     main()
+
